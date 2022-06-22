d9ffd8fb71266ff8e5ee8d3d7290d01db94d97ac
Mycal Tucker <mycal@mit.edu>
2022-06-22 16:55:56 -0400

    Add resnet viz features.
    Adds visualization tools for resnet embeddings, colored by topname.
    Implements MDS and TSNE visualization methods.

---
+++ src/scripts/viz_modemaps.py
@@ -0,0 +1,36 @@
+
+from src.data_utils.read_data import get_feature_data
+from src.utils.plotting import plot_mds, plot_tsne
+import numpy as np
+
+
+def run():
+    data = get_feature_data(features_filename, desired_names=viz_names)
+    features = data['features']
+    names = data['topname']
+    num_per_class = 40
+    regrouped_data = []
+    labels = []
+    for g in np.unique(names):
+        ix = np.where(names == g)[0]
+        max_len = min(num_per_class, len(ix))
+        ix = ix[:max_len]
+        matching_features = np.vstack(features[ix].values)
+        averaged = np.mean(matching_features, axis=0, keepdims=True)
+        plot_features = averaged if plot_mean else matching_features
+        regrouped_data.append(plot_features)
+        labels.append(g)
+    plot_mds(regrouped_data, labels=labels)
+    plot_tsne(regrouped_data, labels=labels)
+
+
+if __name__ == '__main__':
+    # Specify which classes you want to use.
+    # If you don't want to specify, you'll end up with tons of classes.
+    # But you can do so by removing the 'desired_names' field in the call to get_feature_data()
+    viz_names = ['airplane', 'plane',
+                 'animal', 'cow', 'dog', 'cat',
+                 'chair', 'counter', 'table']
+    features_filename = 'data/features_nobox.csv'
+    plot_mean = False  # Do you want to plot all the individual points or the average for each class?
+    run()
--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import euclidean_distances
+from sklearn.manifold import MDS, TSNE
 
 
 def plot_metrics(metrics, labels):
@@ -8,3 +11,46 @@ def plot_metrics(metrics, labels):
     plt.legend()
     plt.savefig('metrics.png')
     plt.close()
+
+
+def plot_mds(all_data, labels=None):
+    mds_embedder = MDS(n_components=2)
+    catted = np.vstack(all_data)
+    if catted.shape[0] > 500:
+        print("Warning, data very long. Truncating")
+        catted = catted[:500]
+    # Hacky, but to visualize all the points around a cluster, we need to add noise to spread them out a tiny bit.
+    # catted = np.random.normal(catted, 0.1)
+    similarities = euclidean_distances(catted.astype(np.float64))
+    transformed = mds_embedder.fit_transform(similarities)
+    x = transformed[:, 0]
+    y = transformed[:, 1]
+    fig, ax = plt.subplots()
+    last_idx = 0
+    for data_idx, data in enumerate(all_data):
+        label = None if labels is None else labels[data_idx]
+        sub_x = x[last_idx: last_idx + len(data)]
+        sub_y = y[last_idx: last_idx + len(data)]
+        pcm = ax.scatter(sub_x, sub_y, s=20, marker='o', label=label)
+        last_idx += len(data)
+    plt.legend()
+    plt.show()
+
+
+def plot_tsne(all_data, labels=None):
+    tsne = TSNE(n_components=2, learning_rate='auto', init='random')
+    # Concat all data
+    catted = np.vstack(all_data)
+    transformed = tsne.fit_transform(catted)
+    x = transformed[:, 0]
+    y = transformed[:, 1]
+    fig, ax = plt.subplots()
+    last_idx = 0
+    for data_idx, data in enumerate(all_data):
+        label = None if labels is None else labels[data_idx]
+        sub_x = x[last_idx: last_idx + len(data)]
+        sub_y = y[last_idx: last_idx + len(data)]
+        pcm = ax.scatter(sub_x, sub_y, s=20, marker='o', label=label)
+        last_idx += len(data)
+    plt.legend()
+    plt.show()
